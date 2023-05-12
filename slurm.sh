748336c16b0000ca6d2b276e1a6d1030cd665fbe
Mustafa Enes Batur <mustafa.enes99@gmail.com>
2023-05-12 21:08:54 +0200

    Update slurm script

--- slurm.sh
+++ slurm.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+sbatch <<EOT
+
 #SBATCH -J %%name%%
 
 # Cluster Settings
@@ -10,3 +13,5 @@ source %%home%%/airhockey/.venv/bin/activate
 export PYTHONPATH="${PYTHONPATH}:%%home%%/airhockey"
 
 python %%home%%/airhockey/run.py _from_slurm $@
+
+EOT
